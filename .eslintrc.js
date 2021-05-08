be78fc3ae5d40a3820389da3be410eba3bb3efca
settyan <senaredline@gmail.com>
2021-05-08 13:55:07 +0900

    Fix ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,10 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
