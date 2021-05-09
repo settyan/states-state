898ad6f6e85d805d2407620fab4c1eec5b5e479a
settyan <senaredline@gmail.com>
2021-05-09 16:48:55 +0900

    run fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,7 +3,11 @@ const rootPath = path.resolve(__dirname, "../")
 
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials",'storybook-css-modules-preset'],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-css-modules-preset",
+  ],
   webpackFinal: async (config) => {
     config.resolve.alias["@"] = `${rootPath}/src`
     return config
