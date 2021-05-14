4be00976acb19a506683f406ceb287b3c6b8c933
settyan <senaredline@gmail.com>
2021-05-14 14:52:20 +0900

    Fix imports

--- src/components/Logo/Logo.stories.tsx
+++ src/components/Logo/Logo.stories.tsx
@@ -2,14 +2,14 @@ import React from "react"
 
 import { Story, Meta } from "@storybook/react"
 
-import { Logo, LogoProps } from "./Logo"
+import { Logo } from "./Logo"
 
 export default {
   title: "Logo",
   component: Logo,
 } as Meta
 
-const Template: Story<LogoProps> = (args) => <Logo {...args} />
+const Template: Story = (args) => <Logo {...args} />
 
 export const Default = Template.bind({})
 Default.args = {}
